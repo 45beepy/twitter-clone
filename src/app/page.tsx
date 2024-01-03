649c3ba525c294b9a1f7bf9d218e51ec5131be47
Pravin <pravin.bp.art@gmail.com>
2024-01-04 00:01:01 +0530

    Update page.tsx
    More improv on sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import {BiHomeCircle,BiUser} from 'react-icons/bi'
 import {BsBell,BsBookmark} from 'react-icons/Bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
 import {HiEnvelope} from 'react-icons/hi2'
+import { RiTwitterXLine } from "react-icons/ri";
 import React from 'react'
 import Link from 'next/link'
 
 const NAVIGATION_ITEMS = [
+  {
+    title:'Twitter',
+    icon: RiTwitterXLine
+  },
   {
     title:'Home',
     icon: BiHomeCircle
@@ -36,27 +41,26 @@ const NAVIGATION_ITEMS = [
 ]
 const page = () => {
   return (
-    <div className='w-full h-full flex justify-centre items-centre relative'>
-      <div className='max-w-screen-lg w-full h-full h-flex relative'>
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-xl w-full h-full h-flex relative">
         {/*left sidebar for navigation/header */}
-          <section className='fixed w-72 flex flex-col'>
+          <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
             {
               NAVIGATION_ITEMS.map((item)=>(
-                <Link className='bg-white/50 flex item-centre justify-centre space-x-2 rounded-3xl p-4' href={`/${item.title.toLowerCase()}`} key={item.title}>
+                <Link className='hover:bg-white/10 text-2xl transition duration-200 flex item-center justify-start w-fit space-x-4 rounded-3xl py-2 px-5' 
+                href={`/${item.title.toLowerCase()}`} key={item.title}>
                   <div>
                     <item.icon/>
                   </div>
                   <div>
-                    {
-                      item.title
-                    }
+                    {item.title !== "Twitter" && item.title }
                   </div>
                 </Link>
               ))
             }
           </section>
-      <main>Home-timeline</main>
-      <section>right-trending</section>
+        {/*<main>Home-timeline</main>
+      <section>right-trending</section>*/}
       </div>
     </div>
 
