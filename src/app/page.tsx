966a1abb7456b5904aa59b26d87aee350b67a28e
Pravin <pravin.bp.art@gmail.com>
2024-02-05 21:41:27 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,27 @@ const page = () => {
         {/*left sidebar for navigation/header */}
           <LeftSidebar />
         <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-          <h1 className="text-3xl font-bold">Home
-          </h1>
+          <h1 className="text-3xl font-bold">Home</h1>
+          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 flex items-strech py-4 space-x-2 border-gray-600 h-32 relative">
+            <div className="w-10 h-10 bg-slate-400 flex-none rounded-full"></div>
+            <div className="flex flex-col w-full">
+                  <input type="text" 
+                  className="w-full h-full bg-transparent border-b-[0.5px p-4 outline-none border-none"
+                  placeholder="What's Happening"/>
+              <div className="w-full justify-between intems-center flex">
+                <div>
+
+                </div>
+                <div className="w-full max-w-[100px]">
+                  <button className='m-2 rounded-full bg-primary px-4 py-2 text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold'>Post</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </main>
       {/*<section>right-trend275ing</section>*/}
       </div>
-      
     </div>
 
   )
