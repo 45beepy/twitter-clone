fd085ea51dd668789d5385168f4798ae0db4d66c
Pravin <pravin.bp.art@gmail.com>
2024-05-17 00:06:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,23 @@ import MainComponent from "./components/MainComponent";
 const page = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-[80vw] h-full flex relative">
+      <div className="max-w-[70vw] w-full h-full flex relative">
           <LeftSidebar />
           <MainComponent />
           <section
-          className="w-full sticky hidden top-2 overflow-y-auto mt-2 xl:flex flex-col items-stretch h-[90vh] overflow-x-hidden px-6">
+          className="w-full sticky top-2 mt-2 flex flex-col items-stretch h-screen px-6">
             <div>
-              <div className="relative w-full h-full">
+              <div className="relative w-full h-full group">
                 <label 
                 htmlFor="searchBox"
-                className="absolute top-0 left-0 h-full flex items-center justify-center">
-                  <BsSearch className="w-5 h-5 text-gray-500"/>
+                className="absolute top-0 left-0 h-full flex items-center justify-center p-4">
+                  <BsSearch className="w-5 h-5 text-gray-500 peer-focus:text-primary"/>
                 </label>
                 <input 
                 id="searchBox"
                 type="text"
                 placeholder="Search Twitter"
-                className="outline-none focus:border-primary bg-transparent border w-full h-full rounded-xl py-4 pl-14 pr-4"
+                className="outline-none focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-4 pl-14 pr-4"
                 />
               </div>
             </div>
