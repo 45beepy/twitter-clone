122246d26f64fdcd617bf1efa0121a9556e793a2
Pravin <pravin.bp.art@gmail.com>
2024-08-19 21:30:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,8 @@ const RightSection = () => {
   return (
     <div className="flex w-full h-full justify-center relative items-center bg-black">
       <div className="max-w-[70vw] w-full h-full flex relative">
-              
-
       <Leftsidebar />
      <MainComponent />
-
      <section className="w-[28%] sticky hidden top-2 overflow-y-auto mt-2 xl:flex flex-col items-stretch h-[90vh] overflow-x-hidden px-6">
        <div>
          <div className="relative w-full h-full group">
